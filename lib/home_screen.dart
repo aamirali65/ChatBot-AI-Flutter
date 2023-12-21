d169350ee40620fb700bdd8b742f29747899c70f
Aamir Almani <aamiraptech39@gmail.com>
2023-12-21 17:30:53 +0500

    Initial commit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -11,17 +11,84 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff010101),
+      backgroundColor: const Color(0xff010101),
       appBar: AppBar(
-        backgroundColor: Color(0xff010101),
-        title: Row(
+        backgroundColor: const Color(0xff010101),
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Hi, Aamir'),
-            Image(image: AssetImage('assets/images/icon.png'),width: 100),
+            Text('Hi, Aamir',style: TextStyle(fontFamily: 'circular'),),
+            SizedBox(width: 10,),
+            Image(image: AssetImage('assets/images/icon.png'),width: 30),
+          ],
+        ),
+        actions: [
+          const CircleAvatar(
+            backgroundImage: AssetImage('assets/images/profile.png'),
+          ),
+          const SizedBox(width: 20,)
+        ],
+      ),
+      drawer: const Drawer(),
+      body: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text('How may I help \nyou today?',style: TextStyle(color: Colors.white,fontSize: 40,fontFamily: 'circular'),),
+            const SizedBox(height: 50,),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('History',style: TextStyle(color: Colors.white,fontSize: 17,fontFamily: 'circular'),),
+                Text('See all',style: TextStyle(color: Colors.grey,fontSize: 14,fontFamily: 'circular'),)
+              ],
+            ),
+            const SizedBox(height: 20,),
+            Container(
+              height: 300,
+              child: ListView.builder(
+                itemCount: 3
+                 , itemBuilder: (context,index){
+                return Container(
+                  margin: EdgeInsets.symmetric(vertical: 5),
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        width: 1,
+                        color: Colors.white38
+                    ),
+                    borderRadius: BorderRadius.circular(30),
+                    color: const Color(0xff171717),
+                  ),
+                  child: ListTile(
+                    titleAlignment: ListTileTitleAlignment.center,
+                    leading: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: const BoxDecoration(
+                          color: Color(0xffC6F432),
+                          shape: BoxShape.circle,
+                          image: DecorationImage(image: AssetImage('assets/images/msg.png'))
+                      ),
+                    ),
+                    title: const Text('I need some UI inspiration for dark...',style: TextStyle(color: Colors.white),textAlign: TextAlign.center,),
+                    trailing: PopupMenuButton(
+                        tooltip: '',
+                        icon:const Icon(Icons.more_vert,color: Colors.white,),
+                        itemBuilder: (context,) =>
+                        const[
+                          PopupMenuItem(value: '1', child: Text('Save')),
+                          PopupMenuItem(value: '2', child: Text('Remove')),
+                        ]),
+                  ),
+                );
+              }),
+            ),
           ],
         ),
       ),
-      drawer: Drawer(),
     );
   }
 }
--- lib/starting-page.dart
+++ lib/starting-page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_ai/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class StartPage extends StatefulWidget {
@@ -11,51 +12,50 @@ class _StartPageState extends State<StartPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff010101),
-      body: Stack(
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(image: AssetImage('assets/images/back.png'),
-                fit: BoxFit.cover
-
-              )
-            ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/main.png'),
+            fit: BoxFit.fill
           ),
-          Container(
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  child: Text('Personal AI Buddy',style: TextStyle(fontWeight: FontWeight.bold,fontFamily: 'circular'),textAlign: TextAlign.center,),
-                  padding: EdgeInsets.symmetric(vertical: 10,horizontal: 20),
-                  decoration: BoxDecoration(
-                      color: Color(0xffC6F432),
+        ),
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.symmetric(vertical: 20),
+          child: Column(
+            children: [
+              Container(
+                child: const Text('Personal AI Buddy',style: TextStyle(fontWeight: FontWeight.bold,fontFamily: 'circular'),textAlign: TextAlign.center,),
+                padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 20),
+                decoration: BoxDecoration(
+                    color: const Color(0xffC6F432),
                     borderRadius: BorderRadius.circular(50)
-                  ),
                 ),
-                SizedBox(
-                  width: double.infinity,
-                  child: Image(image: AssetImage('assets/images/Group.png')),
-                ),
-                Container(
-                    padding: EdgeInsets.symmetric(horizontal: 50)
-                    ,child: Text('How may I help you today!',
-                  style: TextStyle(color: Colors.white,fontSize: 40,fontFamily: 'circular'),textAlign: TextAlign.center,)),
-                SizedBox(height: 20,),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 100,vertical: 20),
-                  child: Text('Get Started'),
+              ),
+              const SizedBox(height: 20,),
+              const Image(image: AssetImage('assets/images/hero1.png')),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Text('How may I help \nyou today!',
+              style: TextStyle(color: Colors.white,fontSize: 40,fontFamily: 'circular'),textAlign: TextAlign.center,),
+          ),
+              const SizedBox(height: 30,),
+              GestureDetector(
+                onTap: (){
+                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>HomeScreen()));
+                },
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 100,vertical: 20),
+                  child: const Text('Get Started',style: TextStyle(fontWeight: FontWeight.bold,fontFamily: 'circular'),),
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.white
                   ),
-                )
-              ],
-            ),
-          )
-        ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
